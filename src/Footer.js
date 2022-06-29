30ea56c5fee42f45ba96f152c513627065d8a563
Dhiraj-j <dhirajjaiswal044@gmail.com>
2022-06-29 03:33:38 -0700

    version 2 commit

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './footer.css';
+
+const Footer = () => {
+  return (
+    <footer className='footer'>
+        
+        <span 
+                  >© 2022 Movie App. Design with ♥️ by Dhiraj Jaiswal.</span>
+    </footer>
+  )
+}
+
+export default Footer;
